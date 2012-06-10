4a3d6fc7140e33d4d9cc178a476eefba638afaa8
Thom Leggett <github@thomleggett.co.uk>
2012-06-10 23:44:16 +0100

    Add setup script. DANGER.

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+rm -f $HOME/.conkerorrc
+ln -s $SCRIPTPATH/.conkerorrc $HOME/.conkerorrc
+
+rm -f $HOME/.dircolors
+ln -s $SCRIPTPATH/.dircolors $HOME/.dircolors
+
+rm -f $HOME/.Xdefaults
+ln -s $SCRIPTPATH/.Xdefaults $HOME/.Xdefaults
+
+rm -f $HOME/.xmobarrc
+ln -s $SCRIPTPATH/.xmobarrc $HOME/.xmobarrc
+
+rm -rf $HOME/.xmonad
+ln -s $SCRIPTPATH/.xmonad $HOME/.xmonad
+
+rm -f $HOME/.xsession
+ln -s $SCRIPTPATH/.xsession $HOME/.xsession
