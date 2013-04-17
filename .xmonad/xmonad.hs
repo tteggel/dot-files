06c7e087c5256a38554b8c8357e6dfa899277343
Thom Leggett <github@thomleggett.co.uk>
2013-04-17 21:34:03 +0100

    Updates for zsh and funtoo environment.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,11 +1,11 @@
 import XMonad
-import qualified XMonad.StackSet as W 
+import qualified XMonad.StackSet as W
 import qualified Data.Map as M
 import XMonad.Util.EZConfig
 import System.Exit
 import Graphics.X11.Xlib
 import System.IO
- 
+
 
 -- actions
 import XMonad.Actions.CycleWS
@@ -13,21 +13,21 @@ import XMonad.Actions.WindowGo
 import qualified XMonad.Actions.Search as S
 import XMonad.Actions.Search
 import qualified XMonad.Actions.Submap as SM
- 
+
 -- utils
 import XMonad.Util.Run(spawnPipe)
-import qualified XMonad.Prompt 		as P
+import qualified XMonad.Prompt          as P
 import XMonad.Prompt.Shell
 import XMonad.Prompt
- 
- 
+
+
 -- hooks
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.FadeInactive
- 
+
 -- layouts
 import XMonad.Layout.NoBorders
 import XMonad.Layout.ResizableTile
@@ -36,7 +36,7 @@ import XMonad.Layout.IM
 import XMonad.Layout.Tabbed
 import XMonad.Layout.PerWorkspace (onWorkspace)
 import XMonad.Layout.Grid
- 
+
 import Control.Monad (liftM2)
 
 import XMonad.Hooks.ICCCMFocus
@@ -63,21 +63,21 @@ myLayoutHook = avoidStruts $  onWorkspace "5:im" (withIM (1%7) (Title "Buddy Lis
 myManageHook = composeAll . concat $
                [[ HMH.composeOne [ isKDEOverride HMH.-?> doFloat ] ]
                 ,[isFullscreen                          --> doFullFloat
-		, className =? "OpenOffice.org 3.1"    --> viewShift "6:office" 
-		, className =? "Xmessage" 	       --> doCenterFloat
+                , className =? "OpenOffice.org 3.1"    --> viewShift "6:office"
+                , className =? "Xmessage"              --> doCenterFloat
                 , className =? "Pidgin"                --> viewShift "5:im"
                 , className =? "Emacs"                 --> viewShift "2:text"
                 , className =? "Shiretoko"             --> viewShift "1:web"
                 , className =? "Chromium-browser"      --> viewShift "1:web"
-                , className =? "URxvt"                 --> viewShift "3:console"
+                , className =? "Konsole"               --> viewShift "3:console"
                 , className =? "Mail"                  --> viewShift "4:mail"]
                 , [className =? c --> doFloat          | c <- myFloats]
-		]
+                ]
 
-		where
-			myIgnores = ["trayer"]
-			myFloats  = ["Tilda"]
-			myOtherFloats = []
+                where
+                        myIgnores = ["trayer"]
+                        myFloats  = ["Tilda"]
+                        myOtherFloats = []
                         viewShift = doF . liftM2 (.) W.greedyView W.shift
 
 myWorkspaces = ["1:web", "2:text", "3:console", "4:mail", "5:im", "6:office", "7:media", "8:misc", "9:misc"]
@@ -86,7 +86,7 @@ myKeys =  [
            ("M-S-l", spawn "gnome-screensaver-command -l")
           ]
           ++
-          [ 
+          [
            (mask ++ "M-" ++ [key], screenWorkspace scr >>= flip whenJust (windows . action))
                | (key, scr)  <- zip "wer" [0,1,2] -- was [0..] *** change to match your screen order ***
                , (action, mask) <- [ (W.view, "") , (W.shift, "S-")]
@@ -98,15 +98,15 @@ myConfig = defaultConfig
         { manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
         , layoutHook = avoidStruts $ smartBorders $ myLayoutHook
         , startupHook = return () >> checkKeymap myConfig myKeys
-        , modMask = mod4Mask     -- Rebind Mod to the Caps Lock key 
+        , modMask = mod4Mask     -- Rebind Mod to the Caps Lock key
         , workspaces = myWorkspaces
-        , terminal = "urxvt"
+        , terminal = "konsole"
         }
         `additionalKeysP` myKeys
 
 main = do
     xmproc <- spawnPipe "/usr/bin/xmobar $HOME/.xmobarrc"
-    xmonad $ withUrgencyHook myUrgencyHook $ myConfig 
+    xmonad $ withUrgencyHook myUrgencyHook $ myConfig
         { logHook = do
             (dynamicLogWithPP $ xmobarPP
                 { ppOutput = hPutStrLn xmproc
@@ -128,5 +128,3 @@ myXmobarStrip = strip [] where
       | '<' == head x          = strip (keep ++ "<") (tail x)
       | otherwise              = let (good,x') = span (/= '<') x
                                  in strip (keep ++ good) x'
-
-
---
+++ .zshrc
@@ -0,0 +1,60 @@
+# Path to your oh-my-zsh configuration.
+ZSH=$HOME/.oh-my-zsh
+
+# Set name of the theme to load.
+# Look in ~/.oh-my-zsh/themes/
+# Optionally, if you set this to "random", it'll load a random theme each
+# time that oh-my-zsh is loaded.
+ZSH_THEME="powerline"
+
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# emacs
+alias en='emacsclient -c -n'
+alias e='emacsclient -n'
+alias et='emacsclient -t'
+alias emacs='emacsclient -a "" -c'
+
+EDITOR='emacsclient -t -n'
+
+# open
+alias open=xdg-open
+
+POWERLINE_RIGHT_A="mixed"
+POWERLINE_HIDE_USER_NAME="true"
+POWERLINE_DETECT_SSH="true"
+POWERLINE_SHOW_GIT_ON_RIGHT="true"
+POWERLINE_HIDE_HOST_NAME="true"
+
+TERM="xterm-256color"
+
+PATH=$PATH:$HOME/bin
+
+# Set to this to use case-sensitive completion
+# CASE_SENSITIVE="true"
+
+# Comment this out to disable bi-weekly auto-update checks
+# DISABLE_AUTO_UPDATE="true"
+
+# Uncomment to change how many often would you like to wait before auto-updates occur? (in days)
+# export UPDATE_ZSH_DAYS=13
+
+# Uncomment following line if you want to disable colors in ls
+# DISABLE_LS_COLORS="true"
+
+# Uncomment following line if you want to disable autosetting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment following line if you want red dots to be displayed while waiting for completion
+# COMPLETION_WAITING_DOTS="true"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+plugins=(git colored-man copydir cp cpanm git-extras gitfast github gnu-utils jira lein pip python ssh-agent svn mercirual themes urltools virtualenvwrapper)
+
+source $ZSH/oh-my-zsh.sh
+
+# Customize to your needs...
