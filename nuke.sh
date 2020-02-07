f90f3b990eab9b59249d4c1058b0ce90760299cc
Thom Leggett <thom@tteggel.org>
2020-02-07 09:49:41 +0000

    copy in nix files

--- nuke.sh
+++ nuke.sh
@@ -97,7 +97,7 @@ mkdir -p "$EFI_MNT" || true
 mount "$EFI_PART" "$EFI_MNT"
 nixos-generate-config --root /mnt
 cp /mnt/etc/nixos/hardware-configuration.nix $SCRIPT_PATH/nix/
-rm -rf /mnt/etc/nixos
+rm -rf /mnt/etc/nixos && mkdir -p /mnt/etc/nixos
 cp -rf -t /mnt/etc/nixos $SCRIPT_PATH/nix/*
 ln -sf /mnt/etc/nixos/machines/engineer.nix /mnt/etc/nixos/machine.nix
 nixos-install --no-root-passwd
