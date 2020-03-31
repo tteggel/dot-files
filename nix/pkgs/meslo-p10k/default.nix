4bcb4d1cfa2fed69c5da29d8950e78c11640653a
Thom Leggett <thom@tteggel.org>
2020-03-31 22:37:35 +0100

    Update default.nix

--- nix/pkgs/meslo-p10k/default.nix
+++ nix/pkgs/meslo-p10k/default.nix
@@ -6,7 +6,7 @@ let
     { name = "MesloLGS NF Regular"; hash = "sha256:0h8v3qlgc4v32xrkjcss6qv0i6nrg6wi2za08lbzk0qmd3a3sg3p"; }
     { name = "MesloLGS NF Bold"; hash = "sha256:162d0z6skq28iv138qdiqxbznyxin78parcicvrb2lsc4nwyyn1m"; }
     { name = "MesloLGS NF Italic"; hash = "sha256:0rz029b3qnijlbl97mrz30ya7kk9grn9h0r35610c70rsic8a4ld"; }
-    { name = "MesloLGS NF Bold Italic"; hash = "sha256:0kzc60xmr7lwzz4h8iabg7k6kr8l096n0ajhi7x88npp3msdfrc7"; }
+    { name = "MesloLGS NF Bold Italic"; hash = "sha256:0g5q6my8k6aaf26sq610v9v17j3gsba63f1wv2yix48sdj3pxvbz"; }
   ];
   baseUrl = https://github.com/romkatv/powerlevel10k-media/raw/master/;
 in
