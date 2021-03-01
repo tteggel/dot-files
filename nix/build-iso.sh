c8d79ad4244f2074f559eca516b1e538b059bd42
Thom Leggett <thom@bookcreator.com>
2021-03-01 11:10:19 +0000

    Remove xtsproxy gubbins

--- nix/build-iso.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-env NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz \
-  nix-build '<nixpkgs/nixos>' --no-out-link --show-trace \
-    -A config.system.build.isoImage \
-    -I nixos-config=iso.nix 
